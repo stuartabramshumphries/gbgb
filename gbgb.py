2d37c7d2e9ddd05139d238fd9eb42f17e4d24ab1
Stuart Abrams-Humphries <stuartabramshumphries@gmail.com>
2015-08-16 11:19:25 +0100

    now does a moving average of finishing position

--- gbgb.py
+++ gbgb.py
@@ -201,6 +201,7 @@ def calc_moving_average(dogname):
         fd3=open("calctime-mvavg.out.csv","a")
         fd3s=open("splits-mvavg.out.csv","a")
         fd3wt=open("winnerstime-mvavg.out.csv","a")
+        fd3po=open("position-mvavg.out.csv","a")
     except:
         pass
     dat=fd.readlines()
@@ -208,6 +209,7 @@ def calc_moving_average(dogname):
     data_calctime=[]
     data_brkn=[]
     data_wint=[]
+    data_pos=[]
     cnt=1
     # format of line is
     # 12 columns - from left to right remember 1st is pos 0 last is pos 11
@@ -223,9 +225,7 @@ def calc_moving_average(dogname):
             brkn=float(brk)
             data_brkn.append(brkn)
             grade=splitline[10]
-            #pos=pos[:-2]
             pos=int(pos)
-            print  pos
             going=splitline[4]
             wint=splitline[7]
             calt=splitline[11]
@@ -236,6 +236,7 @@ def calc_moving_average(dogname):
             winnerstime = float(wint)
             rat=ratings[grade][pos]
             datal=dogname+","+grade+"\n"
+            data_pos.append(pos)
             if calctime != 0:
                 data_calctime.append(calctime)
             if winnerstime != 0:
@@ -260,29 +261,36 @@ def calc_moving_average(dogname):
     klist3=[round(elem,2) for elem in klist3]
     klist4=list(movingaverage(data_wint,period))
     klist4=[round(elem,2) for elem in klist4]
+    klist5=list(movingaverage(data_pos,period))
+    klist5=[round(elem,2) for elem in klist5]
     dogname=dogname.replace('%20','+')
     v=(dogname,klist)
     v1=(dogname,klist1)
     v2=(dogname,klist2)
     v3=(dogname,klist3)
     v4=(dogname,klist4)
+    v5=(dogname,klist5)
     value=str(v)
     value1=str(v1)
     value2=str(v2)
     value3=str(v3)
     value4=str(v4)
+    value5=str(v5)
     fd2.write(value)
     fd3.write(value2)
     fd3s.write(value3)
     fd3wt.write(value4)
+    fd3po.write(value5)
     fd2.write("\n")
     fd3.write("\n")
     fd3s.write("\n")
     fd3wt.write("\n")
+    fd3po.write("\n")
     fd2.close()
     fd3.close()
     fd3s.close()
     fd3wt.close()
+    fd3po.close()
 
 if __name__ == '__main__':
     # maybe just maybe put some error checking here
